136f213a40b5d7de9001d9947a729efca1c5a6d7
Thomas Ball <thomasjball@users.noreply.github.com>
2025-12-05 14:58:42 -0800

    Initial files for MakeCode project

---
+++ tilemap.g.ts
@@ -0,0 +1,134 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile3 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile5 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile4 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile6 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile7 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level4":
+            case "level4":return tiles.createTilemap(hex`1000100002020202010101010101010102020202020202020101010101010101020202020202020201010101010101010202020202020202010101010101010102020202020202020101010101010101020202020202020201010101010101010202020202020202010101010101010102020202020202020101010101010101020202020202020201010101010101010202020202020202010101010101010102020202020202020101010101010101020202020202020201010101010101010202020202020202010101010101010102020202020202020101010101010101020202020202020201010101010101010202020202020202020202020202020202020202`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16,myTiles.tile1,myTiles.tile5], TileScale.Sixteen);
+            case "level7":
+            case "level7":return tiles.createTilemap(hex`110040000607070400000000000000000006070704010a0b020000000000000000000d0a0b0201090c020000000000000000000d090c02010a0b020000000000000000000d0a0b0201090c020000000000000000000d090c02010a0b020000000000000000000d0a0b0201090c020000000000000000000d090c02010a0b020000000000000000000d0a0b0201090c020000000000000000000d090c02010a0b020000000000000000000d0a0b0201090c020000000000000000000d090c02010a0b020000000000000000000d0a0b0201090c020000000000000000000d090c02010a0b020000000000000000000d0a0b0201090c020000000000000000000d090c02010a0b020000000000000000000d0a0b0201090c020000000000000000000d090c02010a0b020000000000000000000d0a0b0201090c020000000000000000000d090c02010a0b020000000000000000000d0a0b0201090c020000000000000000000d090c02010a0b020000000000000000000d0a0b0201090c020000000000000000000d090c02010a0b020000000000000000000d0a0b0201090c020000000000000000000d090c02010a0b020000000000000000000d0a0b0201090c020000000000000000000d090c02010a0b020000000000000000000d0a0b0201090c020000000000000000000d090c02010a0b020000000000000000000d0a0b0201090c020000000000000000000d090c02030a0b020000000000000000000d0a0b050d090c020000000000000000000d090c020d0a0b020000000000000000000d0a0b020d090c020000000000000000000d090c020d0a0b020000000000000000000d0a0b020d090c020000000000000000000d090c020d0a0b020000000000000000000d0a0b020d090c020000000000000000000d090c020d0a0b020000000000000000000d0a0b020d090c020000000000000000000d090c020d0a0b020000000000000000000d0a0b020d090c020000000000000000000d090c020d0a0b020000000000000000000d0a0b020d090c020000000000000000000d090c020d0a0b020000000000000000000d0a0b020d090c020000000000000000000d090c020d0a0b020000000000000000000d0a0b020d090c020000000000000000000d090c020d0a0b020000000000000000000d0a0b020d090c020000000000000000000d090c020d0a0b020000000000000000000d0a0b020d090c020000000000000000000d090c020d0a0b020000000000000000000d0a0b020d090c020000000000000000000d090c020d0a0b020000000000000000000d0a0b020d090c020000000000000000000d090c020d0a0b020000000000000000000d0a0b020d090c020000000000000000000d090c020d0a0b020000000000000000000d0a0b020d090c020000000000000000000d090c020d0a0b020000000000000000000d0a0b020d090c020000000000000000000d090c020308080500000000000000000003080805`, img`
+2222.........2222
+2222.........2222
+2222.........2222
+2222.........2222
+2222.........2222
+2222.........2222
+2222.........2222
+2222.........2222
+2222.........2222
+2222.........2222
+2222.........2222
+2222.........2222
+2222.........2222
+2222.........2222
+2222.........2222
+2222.........2222
+2222.........2222
+2222.........2222
+2222.........2222
+2222.........2222
+2222.........2222
+2222.........2222
+2222.........2222
+2222.........2222
+2222.........2222
+2222.........2222
+2222.........2222
+2222.........2222
+2222.........2222
+2222.........2222
+2222.........2222
+2222.........2222
+2222.........2222
+2222.........2222
+2222.........2222
+2222.........2222
+2222.........2222
+2222.........2222
+2222.........2222
+2222.........2222
+2222.........2222
+2222.........2222
+2222.........2222
+2222.........2222
+2222.........2222
+2222.........2222
+2222.........2222
+2222.........2222
+2222.........2222
+2222.........2222
+2222.........2222
+2222.........2222
+2222.........2222
+2222.........2222
+2222.........2222
+2222.........2222
+2222.........2222
+2222.........2222
+2222.........2222
+2222.........2222
+2222.........2222
+2222.........2222
+2222.........2222
+2222.........2222
+`, [myTiles.transparency16,sprites.dungeon.greenOuterWest0,sprites.dungeon.greenOuterEast0,sprites.dungeon.greenOuterSouthEast,sprites.dungeon.greenOuterNorthEast,sprites.dungeon.greenOuterSouthWest,sprites.dungeon.greenOuterNorthWest,sprites.dungeon.greenOuterNorth0,sprites.dungeon.greenOuterSouth0,sprites.dungeon.greenInnerSouthWest,sprites.dungeon.greenInnerNorthWest,sprites.dungeon.greenInnerNorthEast,sprites.dungeon.greenInnerSouthEast,sprites.dungeon.greenOuterWest1], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+            case "myTile0":
+            case "tile2":return tile2;
+            case "myTile1":
+            case "tile3":return tile3;
+            case "myTile":
+            case "tile1":return tile1;
+            case "myTile3":
+            case "tile5":return tile5;
+            case "myTile2":
+            case "tile4":return tile4;
+            case "myTile4":
+            case "tile6":return tile6;
+            case "myTile5":
+            case "tile7":return tile7;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
